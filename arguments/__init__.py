c4459c9624a71ba5eb91e25ccb25eda4da6c0a38
anntsuki <ishaespgtmrv@gmail.com>
2026-01-02 16:15:19 +0800

    2

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -96,7 +96,6 @@ class OptimizationParams(ParamGroup):
         self.depth_l1_weight_init = 1.0
         self.depth_l1_weight_final = 0.01
         self.random_background = False
-        self.not_quantization_aware = False
         self.optimizer_type = "default"
         super().__init__(parser, "Optimization Parameters")
 
@@ -130,7 +129,7 @@ class CompressionParams(ParamGroup):
         self.start_checkpoint = ""
         super().__init__(parser, "Compression Parameters")
 
-
+        
 def get_combined_args(parser : ArgumentParser):
     cmdlne_string = sys.argv[1:]
     cfgfile_string = "Namespace()"
